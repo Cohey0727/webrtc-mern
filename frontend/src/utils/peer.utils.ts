393e50067ae11837de1368994840a9af3992c8fa
Kohei <ohayousagi.ac.kook0727@gmail.com>
2023-07-31 21:53:06 +0900

    main

--- frontend/src/utils/peer.utils.ts
+++ frontend/src/utils/peer.utils.ts
@@ -1,12 +1,12 @@
 import Peer from "simple-peer";
 
 const getOwnSignal = (
-  mediaStream: MediaStream,
+  stream: MediaStream,
 ): Promise<{ peer: Peer.Instance; signal: Peer.SignalData }> => {
   const peer = new Peer({
     initiator: true,
     trickle: false,
-    stream: mediaStream,
+    stream,
   });
   return new Promise((resolve) => {
     peer.on("signal", (signal) => {
